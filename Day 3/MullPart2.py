b0067cbaa8b134c531f487240da9d57851105366
Arriza Wibowo <arrizafathi@gmail.com>
2024-12-06 15:39:01 -0500

    Switch to pipeline to handle multiple regex

--- Day 3/MullPart2.py
+++ Day 3/MullPart2.py
@@ -2,31 +2,25 @@ import re
 
 def analyze_memory():
     """
-    Reads a file containing corrupted memory, extracts valid mul instructions, 
-    and computes the sum of their results.
+    To capture multiple strings in the order of occurance in the txt file, use "|" which means "or". In this context, if you use it on multiple patterns,
+    it will go mul|do|dont, creating a big pattern that contains all of them yet seperate.
     """
 
         # Open and read the file content
     with open("Day 3/MullData.txt", 'r') as file:
-            corrupted_memory = file.read()
+        corrupted_memory = file.read()
         
     # Regular expression parsing to match valid mul instructions
-    pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+    mul = r"mul\((\d{1,3}),(\d{1,3})\)"
     do = r"(do\(\))"
     dont = r"(don't\(\))"
     # Added do and don't regular expressions to capture
 
-    # Find all matches of the valid mul instructions
-    matches = re.findall(pattern, corrupted_memory)
-    do_matches = re.findall(do, corrupted_memory)
-    dont_matches = re.findall(dont, corrupted_memory)
-    print(matches)
-    print(do_matches)
-    print(dont_matches)
-        
-    # Compute the sum of the results of valid mul instructions
-    #result_sum = sum(int(x) * int(y) for x, y in matches)
+    patterns_to_match = [do, dont, mul]
+    pattern = '|'.join(patterns_to_match)
 
-    #print(result_sum)
+
+    matches = re.findall(pattern, corrupted_memory)
+    print(matches)
 
 analyze_memory()
